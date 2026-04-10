cc5bd3a3f39f3ec7b7d6cb4bce680524c3869d94
Mohit Gowda <mohitsmohan5@gmail.com>
2026-04-10 10:31:45 +0530

    Updating the integration test file

--- app/src/server.js
+++ app/src/server.js
@@ -2,6 +2,10 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Lift-Please API running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Lift-Please API running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
